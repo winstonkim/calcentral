bcc428e40fcd05926d90a09f05296ffc84e2d57b
Chris Tweney <chris@media.berkeley.edu>
2012-08-25 19:13:55 -0700

    Add integration test for gitInfo feed

---
+++ src/test/java/edu/berkeley/calcentral/controllers/GitInfoIT.java
@@ -0,0 +1,27 @@
+package edu.berkeley.calcentral.controllers;
+
+import edu.berkeley.calcentral.IntegrationTest;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.jboss.resteasy.util.HttpResponseCodes;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class GitInfoIT extends IntegrationTest {
+
+	@Override
+	protected void setup() {
+		// no-op
+	}
+
+	@Test
+	public void get() throws IOException, JSONException {
+		GetMethod get = doGet("/api/gitInfo");
+		assertResponse(HttpResponseCodes.SC_OK, get);
+		JSONObject json = toJSONObject(get);
+		assertNotNull(json.getJSONObject("gitInfo"));
+	}
+
+}
