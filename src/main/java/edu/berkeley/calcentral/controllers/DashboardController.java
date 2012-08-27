0bb5f82830f9cde2bdd0e90e011772445184d6a7
Yu-Hung Lin <yuhung@yuhunglin.com>
2012-08-27 16:28:08 -0700

    Switching controller to use 'ModelAndView' response

--- src/main/java/edu/berkeley/calcentral/controllers/DashboardController.java
+++ src/main/java/edu/berkeley/calcentral/controllers/DashboardController.java
@@ -13,6 +13,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.google.common.collect.Maps;
 
 import edu.berkeley.calcentral.domain.CalCentralUser;
 import edu.berkeley.calcentral.services.UserDataService;
@@ -22,31 +25,30 @@ import edu.berkeley.calcentral.services.UserDataService;
  */
 @Controller
 public class DashboardController {
-	
+
     @Autowired
     private UserDataService userDataService;
-    
-	/**
-	 * GET call for the dashboard. 
-	 * 
-	 * @param model map to return to the view.
-	 * @param request servlet request object.
-	 * @return dashboard view.
-	 */
-	@PreAuthorize("hasRole('ROLE_USER')")
-	@RequestMapping(value = { "/dashboard" }, method = RequestMethod.GET)
-	public String getDashboard(
-			Map<String, Object> model,
-			HttpServletRequest request) {
-		String uid = request.getUserPrincipal().getName();
-		CalCentralUser user = userDataService.get(uid);
-		String username = "";
-		if (user != null) {
-		    username = new StringBuffer(user.getFirstName()).append(" ").append(user.getLastName()).toString();
-		}
-		
-		model.put("uid", uid);
-		model.put("name", username);
-		return "dashboard";
+
+    /**
+     * GET call for the dashboard. 
+     * 
+     * @param model map to return to the view.
+     * @param request servlet request object.
+     * @return dashboard view.
+     */
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @RequestMapping(value = { "/dashboard" }, method = RequestMethod.GET)
+    public ModelAndView getDashboard(HttpServletRequest request) {
+	String uid = request.getUserPrincipal().getName();
+	CalCentralUser user = userDataService.get(uid);
+	String username = "";
+	if (user != null) {
+	    username = new StringBuffer(user.getFirstName()).append(" ").append(user.getLastName()).toString();
 	}
+	Map<String, Object> model = Maps.newHashMap();
+	model.put("uid", uid);
+	model.put("name", username);
+	
+	return new ModelAndView("dashboard", model);
+    }
 }
