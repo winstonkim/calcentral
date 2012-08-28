c9508217b7007893dc16378a799896e82ed0b1f9
Chris Tweney <chris@media.berkeley.edu>
2012-08-28 13:46:30 -0700

    Make the UserAuthorizationTest more interesting

--- src/test/java/edu/berkeley/calcentral/services/UserAuthorizationTest.java
+++ src/test/java/edu/berkeley/calcentral/services/UserAuthorizationTest.java
@@ -18,31 +18,19 @@
 
 package edu.berkeley.calcentral.services;
 
-import edu.berkeley.calcentral.daos.UserServiceDao;
-import org.junit.Before;
+import edu.berkeley.calcentral.DatabaseAwareTest;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 
-@RunWith(MockitoJUnitRunner.class)
-public class UserAuthorizationTest {
+public class UserAuthorizationTest extends DatabaseAwareTest {
 
+	@Autowired
 	private UserAuthorization userAuthorization;
-
-	@Mock
-	private UserServiceDao iUserAuthorizationDao;
-
-	@Before
-	public void setUp() throws Exception {
-		this.userAuthorization = new UserAuthorization();
-		this.userAuthorization.setUserServiceDao(this.iUserAuthorizationDao);
-	}
 
 	@Test
 	public void testLoadUserByUsername() throws Exception {
-		this.userAuthorization.loadUserByUsername("joe");
-		Mockito.verify(iUserAuthorizationDao).getUserDetails("joe");
+		UserDetails details = this.userAuthorization.loadUserByUsername("2040");
+		assertNotNull(details);
 	}
 }
