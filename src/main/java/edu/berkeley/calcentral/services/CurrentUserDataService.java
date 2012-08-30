cef21a18266ae44b9a0aee246440ffffc7ba963a
Chris Tweney <chris@media.berkeley.edu>
2012-08-30 11:25:39 -0700

    CLC-78 Use a Map in CurrentUserService and eliminate CurrentUser composite bean

--- src/main/java/edu/berkeley/calcentral/services/CurrentUserDataService.java
+++ src/main/java/edu/berkeley/calcentral/services/CurrentUserDataService.java
@@ -4,20 +4,20 @@
  */
 package edu.berkeley.calcentral.services;
 
+import com.google.common.collect.Maps;
+import edu.berkeley.calcentral.Urls;
+import edu.berkeley.calcentral.daos.UserDataDao;
+import edu.berkeley.calcentral.domain.UserData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import edu.berkeley.calcentral.Urls;
-import edu.berkeley.calcentral.daos.UserDataDao;
-import edu.berkeley.calcentral.domain.CurrentUser;
-import edu.berkeley.calcentral.domain.UserData;
+import java.util.Map;
 
 @Service
 @Path(Urls.CURRENT_USER)
@@ -28,16 +28,16 @@ public class CurrentUserDataService {
 
 	@GET
 	@Produces({MediaType.APPLICATION_JSON})
-	public CurrentUser getCurrentUser(
-			@Context HttpServletRequest request) {
-
-		if (request.getUserPrincipal() == null) {
-			return new CurrentUser();
+	public Map<String, Object> getCurrentUser(@Context HttpServletRequest request) {
+		Map<String, Object> result = Maps.newHashMap();
+		if (request.getUserPrincipal() != null) {
+			String uid = request.getUserPrincipal().getName();
+			UserData userData = userDataDao.getUserAndWidgetData(uid);
+			result.put("currentUser", userData);
+		} else {
+			result.put("currentUser", Maps.newHashMap());
 		}
-		String uid = request.getUserPrincipal().getName(); 
-		UserData user = userDataDao.getUserAndWidgetData(uid);
-		CurrentUser currentUser = new CurrentUser(user);
-		return currentUser;
+		return result;
 	}
 
 }
