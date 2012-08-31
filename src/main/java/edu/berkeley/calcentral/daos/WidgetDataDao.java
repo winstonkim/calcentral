8c78704d233bee7f76f8d1e19d1609a6b4b8612e
Chris Tweney <chris@media.berkeley.edu>
2012-08-31 16:57:16 -0700

    Inline some sql statements

--- src/main/java/edu/berkeley/calcentral/daos/WidgetDataDao.java
+++ src/main/java/edu/berkeley/calcentral/daos/WidgetDataDao.java
@@ -21,20 +21,33 @@ public class WidgetDataDao extends BaseDao {
 		params.put("uid", widgetData.getUid());
 		params.put("widgetId", widgetData.getWidgetID());
 		params.put("data", widgetData.getData());
-		queryRunner.update(SqlQueries.saveWidgetData, params);
+		String saveWidgetData = "WITH upsert as "
+				+ " (UPDATE calcentral_widgetdata wd "
+				+ "  SET data = :data "
+				+ "  WHERE wd.uid = :uid AND wd.widgetId = :widgetId RETURNING wd.*), "
+				+ " entries (uid, widgetId, data) as (values (:uid, :widgetId, :data)) "
+				+ " INSERT INTO calcentral_widgetdata (uid, widgetId, data) "
+				+ " SELECT uid, widgetId, data FROM entries "
+				+ " WHERE NOT EXISTS (SELECT 1 FROM upsert up "
+				+ " WHERE up.uid = entries.uid AND up.widgetId = entries.widgetId)";
+		queryRunner.update(saveWidgetData, params);
 	}
 
 	public final void deleteWidgetData(String userId, String widgetId) {
 		Map<String, String> params = Maps.newHashMap();
 		params.put("uid", userId);
 		params.put("widgetId", widgetId);
-		queryRunner.update(SqlQueries.delete, params);
+		String delete = "DELETE FROM calcentral_widgetdata wd "
+				+ " WHERE wd.uid = :uid AND wd.widgetId = :widgetId";
+		queryRunner.update(delete, params);
 	}
 
 	public void deleteAllWidgetData(String userID) {
 		Map<String, String> params = Maps.newHashMap();
 		params.put("uid", userID);
-		queryRunner.update(SqlQueries.deleteAll, params);
+		String deleteAll = "DELETE FROM calcentral_widgetdata wd "
+				+ " WHERE wd.uid = :uid";
+		queryRunner.update(deleteAll, params);
 	}
 
 	public final WidgetData getWidgetData(String userId, String widgetId) {
@@ -43,7 +56,11 @@ public class WidgetDataDao extends BaseDao {
 		params.put("widgetId", widgetId);
 		WidgetData result;
 		try {
-			result = queryRunner.queryForObject(SqlQueries.get, params, new BeanPropertyRowMapper<WidgetData>(WidgetData.class));
+			String get = "SELECT wd.uid, wd.widgetId, wd.data "
+					+ " FROM calcentral_widgetdata wd "
+					+ " WHERE wd.uid = :uid"
+					+ "   AND wd.widgetId = :widgetId";
+			result = queryRunner.queryForObject(get, params, new BeanPropertyRowMapper<WidgetData>(WidgetData.class));
 		} catch (EmptyResultDataAccessException e) {
 			result = null;
 		}
@@ -53,43 +70,14 @@ public class WidgetDataDao extends BaseDao {
 	public final List<WidgetData> getAllWidgetData(String userId) {
 		Map<String, String> params = Maps.newHashMap();
 		params.put("uid", userId);
-		List<WidgetData> result = queryRunner.query(SqlQueries.getAllForUser, params, new BeanPropertyRowMapper<WidgetData>(WidgetData.class));
+		String getAllForUser = "SELECT wd.uid, wd.widgetId, wd.data "
+				+ " FROM calcentral_widgetdata wd "
+				+ " WHERE wd.uid = :uid";
+		List<WidgetData> result = queryRunner.query(getAllForUser, params, new BeanPropertyRowMapper<WidgetData>(WidgetData.class));
 		if (result.isEmpty()) {
 			return null;
 		}
 		return result;
 	}
-
-	private static class SqlQueries {
-		static String getAllForUser = 
-				"   SELECT wd.uid, wd.widgetId, wd.data "
-						+ " FROM calcentral_widgetdata wd "
-						+ " WHERE wd.uid = :uid";
-
-		static String get = 
-				"   SELECT wd.uid, wd.widgetId, wd.data "
-						+ " FROM calcentral_widgetdata wd "
-						+ " WHERE wd.uid = :uid"
-						+ "   AND wd.widgetId = :widgetId";
-
-		static String saveWidgetData = 
-				"   WITH upsert as "
-						+ " (UPDATE calcentral_widgetdata wd "
-						+ "  SET data = :data "
-						+ "  WHERE wd.uid = :uid AND wd.widgetId = :widgetId RETURNING wd.*), "
-						+ " entries (uid, widgetId, data) as (values (:uid, :widgetId, :data)) "
-						+ " INSERT INTO calcentral_widgetdata (uid, widgetId, data) "
-						+ " SELECT uid, widgetId, data FROM entries "
-						+ " WHERE NOT EXISTS (SELECT 1 FROM upsert up "
-						+ " WHERE up.uid = entries.uid AND up.widgetId = entries.widgetId)";
-
-		static String delete = 
-				" DELETE FROM calcentral_widgetdata wd "
-						+ " WHERE wd.uid = :uid AND wd.widgetId = :widgetId";
-
-		static String deleteAll = " DELETE FROM calcentral_widgetdata wd "
-				+ " WHERE wd.uid = :uid";
-	}
-
 
 }
