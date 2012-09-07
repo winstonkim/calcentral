b412cbf51d77830298b546e4942beaa10920a881
Chris Tweney <chris@media.berkeley.edu>
2012-09-07 14:30:13 -0700

    CLC-135 Rewrite 2 tests as database-aware unit tests

--- src/test/java/edu/berkeley/calcentral/services/Sakai2ProxyIT.java
+++ src/test/java/edu/berkeley/calcentral/services/Sakai2ProxyIT.java
@@ -18,43 +18,39 @@
 
 package edu.berkeley.calcentral.services;
 
-import edu.berkeley.calcentral.IntegrationTest;
-import edu.berkeley.calcentral.Urls;
-import org.apache.commons.httpclient.methods.GetMethod;
+import edu.berkeley.calcentral.DatabaseAwareTest;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
-import org.jboss.resteasy.util.HttpResponseCodes;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
+import java.util.Map;
 
-public class Sakai2ProxyIT extends IntegrationTest {
+public class Sakai2ProxyIT extends DatabaseAwareTest {
 
-	@Override
-	protected void setup() {
-		// no-op
-	}
+	private static final Log LOGGER = LogFactory.getLog(WidgetDataServiceTest.class);
+
+	@Autowired
+	private Sakai2Proxy proxy;
 
 	@Test
 	public void get() throws IOException, JSONException {
-		GetMethod get = doGet(Urls.BSPACE_FAVORITES);
-		assertResponse(HttpResponseCodes.SC_OK, get);
-		JSONObject json = toJSONObject(get);
-		logger.debug(json.toString(2));
-		assertNotNull(json.get("body"));
-		assertNotNull(json.get("statusCode"));
-		assertNotNull(json.get("statusText"));
+		Map<String, Object> response = proxy.get(randomString(), "/sakai-hybrid/sites?categorized=true");
+		assertNotNull(response);
+		assertNotNull(response.get("body"));
+		assertNotNull(response.get("statusCode"));
+		assertNotNull(response.get("statusText"));
 	}
 
 	@Test
 	public void getUnread() throws IOException, JSONException {
-		GetMethod get = doGet(Urls.BSPACE_FAVORITES_UNREAD);
-		assertResponse(HttpResponseCodes.SC_OK, get);
-		JSONObject json = toJSONObject(get);
-		logger.debug(json.toString(2));
-		assertNotNull(json.get("body"));
-		assertNotNull(json.get("statusCode"));
-		assertNotNull(json.get("statusText"));
+		Map<String, Object> response = proxy.get(randomString(), "/sakai-hybrid/sites?unread=true");
+		assertNotNull(response);
+		assertNotNull(response.get("body"));
+		assertNotNull(response.get("statusCode"));
+		assertNotNull(response.get("statusText"));
 	}
 
 }
