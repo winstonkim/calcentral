bd04a3e5e5d29fa785d8d1a00d284c4da621fb22
Yu-Hung Lin <yuhung@yuhunglin.com>
2012-09-06 18:04:31 -0700

    [CLC-122] Initial implementation, some explainations on the wierd implmentation in the ticket here: https://jira.media.berkeley.edu/jira/browse/CLC-122

--- src/main/webapp/widgets/canvascourses/javascript/canvascourses.js
+++ src/main/webapp/widgets/canvascourses/javascript/canvascourses.js
@@ -8,6 +8,8 @@ calcentral.Widgets.canvascourses = function(tuid) {
 
 	var $rootel = $('#' + tuid);
 	var $canvascoursesList = $('.cc-widget-canvascourses-list', $rootel);
+	var dummy = false;
+	var accountID = 90242;
 
 	////////////////////
 	// Event Handlers //
@@ -31,12 +33,53 @@ calcentral.Widgets.canvascourses = function(tuid) {
 	// Ajax Requests //
 	///////////////////
 
-	var loadCourses = function() {
+	var getCanvasData = function(url) {
 		return $.ajax({
-			'cache': false,
-			'url': '/widgets/canvascourses/dummy/canvascourses.json'
+			'url': url
 		});
+	}
+
+
+	var loadCourses = function(data) {
+		if (dummy) {
+			return $.ajax({
+				'cache': false,
+				'url': '/widgets/canvascourses/dummy/canvascourses.json'
+			});
+		} else {
+			var $loadCoursesDeferred = $.Deferred();
+			$.when(getCanvasData(data.enrollment_url), getCanvasData(data.courses_url)).done(function(user_enrollment, allCourses){
+			var renderData = [];
+				var courseIds = [];
+				$.each(user_enrollment[0], function(index, value) {
+					courseIds.push(value.course_id);
+				});
+
+				$.grep(allCourses[0], function(course, index) {
+					if ($.inArray(course.id, courseIds) > -1) {
+						renderData.push({
+							'name' : course.course_code + ": " + course.name,
+							'id': course.id
+						});
+					}
+				});
+				$loadCoursesDeferred.resolve(renderData);
+			});
+			return $loadCoursesDeferred.promise();
+		}
 	};
+
+	var getCurrentUserData = function() {
+		var $currentUserDeferred = $.Deferred();
+		calcentral.Api.User.getCurrentUser('', function(success, data) {
+			$currentUserDeferred.resolve({
+				'uid': data.uid,
+				'enrollment_url': '/api/canvas/users/sis_user_id:' + data.uid + '/enrollments',
+				'courses_url': '/api/canvas/accounts/' + accountID + '/courses'
+			});
+		});
+		return $currentUserDeferred.promise();
+	}
 
 	////////////////////
 	// Initialisation //
@@ -46,7 +89,10 @@ calcentral.Widgets.canvascourses = function(tuid) {
 	 * Initialise the canvas classes widget
 	 */
 	var doInit = function(){
-		$.when(loadCourses()).done(renderCourses);
+		$.when(getCurrentUserData()).done(function(userData) {
+			$.when(loadCourses(userData)).done(renderCourses);
+		});
+
 	};
 
 	// Start the request
