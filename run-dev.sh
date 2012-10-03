de82c0977678b2adce60163947da4c61f21e0fd8
Christian Vuerings <vueringschristian@gmail.com>
2012-10-03 14:31:34 -0700

    CLC-368 - Have a 1 line setup for developers https://jira.media.berkeley.edu/jira/browse/CLC-368

---
+++ run-dev.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# script to run when developing for Calcentral
+
+pg_ctl -D /usr/local/var/postgres status > /dev/null 2>&1
+if [ "$?" -gt "0" ]; then
+	echo "PostgreSQL not running yet, we're starting it right now"
+	pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start
+else
+	echo "PostgreSQL is already running"
+fi
+
+echo "Cleaning up the database and doing the migration"
+mvn flyway:clean flyway:migrate
+
+echo "Building Calcentral and adding the Canvas Course Ids"
+mvn clean install -P load-canvas-course-ids exec:java -Dspring.profiles.active=default
+
+echo "Starting the Jetty server"
+mvn jetty:run
