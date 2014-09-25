6678fc2696e668af53468b87e877fb5e3f2cb9cd
Christian Vuerings <vueringschristian@gmail.com>
2014-09-24 18:14:22 -0700

    PIP2-43 - Add support for Twillio messages
    https://jira.berkeley.edu/browse/PIP2-43

--- app/assets/javascripts/angular/controllers/widgets/waitlistController.js
+++ app/assets/javascripts/angular/controllers/widgets/waitlistController.js
@@ -4,11 +4,33 @@
   /**
    * Waitlist controller
    */
-  angular.module('calcentral.controllers').controller('WaitlistController', function(waitlistFactory, $interval, $scope) {
+  angular.module('calcentral.controllers').controller('WaitlistController', function(waitlistFactory, $interval, $http, $scope) {
+
+    var currentWaitlistInfo = {};
+
+    var checkNofitications = function(data) {
+
+      if (currentWaitlistInfo &&
+        currentWaitlistInfo.courses &&
+        data &&
+        data.courses &&
+        data.courses.length === 0 &&
+        currentWaitlistInfo.courses.length !== data.courses.length) {
+
+        var message = 'Congrats Oski, you are now enrolled for ' + currentWaitlistInfo.courses[0].courseName + ' - ' + currentWaitlistInfo.courses[0].courseTitle;
+        $http.get('http://twiltestberkeley.herokuapp.com/message/' + message);
+      }
+
+      currentWaitlistInfo = data;
+    };
 
     var getWaitlist = function() {
       waitlistFactory.getWaitlist().then(function(xhr) {
         angular.extend($scope, xhr.data);
+
+        if (xhr.data && xhr.data.waitlist) {
+          checkNofitications(xhr.data.waitlist);
+        }
       });
     };
 
