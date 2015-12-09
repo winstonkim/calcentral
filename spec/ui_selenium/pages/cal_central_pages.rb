83eae2827fcf0034113f2e444f5d2a0b303362d3
Paul Farestveit <pfarestveit@berkeley.edu>
2015-12-09 11:50:09 -0800

    Fix Google connection test

--- spec/ui_selenium/pages/cal_central_pages.rb
+++ spec/ui_selenium/pages/cal_central_pages.rb
@@ -15,6 +15,7 @@ module CalCentralPages
   button(:email_badge, :xpath => '//button[@title="bMail"]')
   div(:email_count, :xpath => '//button[@title="bMail"]/div[@data-ng-bind="badge.count"]')
   h4(:unread_email_heading, :xpath => '//h4[text()="Unread bMail Messages"]')
+  h4(:email_not_connected_heading, :xpath => '//h4[text()="bMail (Not connected)"]')
   link(:email_one_link, :xpath => '//button[@title="bMail"]/following-sibling::div//a[@data-ng-href="http://bmail.berkeley.edu/"]')
   div(:email_one_sender, :xpath => '//button[@title="bMail"]/following-sibling::div//div[@data-ng-bind="item.editor"]')
   span(:email_one_subject, :xpath => '//button[@title="bMail"]/following-sibling::div//span[@data-ng-bind="item.title"]')
