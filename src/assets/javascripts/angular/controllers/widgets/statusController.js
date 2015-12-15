c4ac41e32c6c51b076a73ed2c4ab85e6a9c9194d
Christian Vuerings <vueringschristian@gmail.com>
2015-12-14 19:36:58 -0800

    SISRP-7618 - Front End Dev: account switcher UI
    https://jira.berkeley.edu/browse/SISRP-7618
    
    SISRP-7698 - Front-End Dev: Service Indicators "Holds": Placement and design(s)
    
    https://jira.berkeley.edu/browse/SISRP-7698
    
    Other things this PR fixes:
     * Show the correct amount for the service indicators
     * Show the start term when this is no start date
     * Fix alignment of icons in the popover
     * Use the correct `showSisProfileUI`
     * Add holds to the popover
     * Allow for multiline instructions in the service indicators

--- src/assets/javascripts/angular/controllers/widgets/statusController.js
+++ src/assets/javascripts/angular/controllers/widgets/statusController.js
@@ -6,7 +6,7 @@ var angular = require('angular');
 /**
  * Status controller
  */
-angular.module('calcentral.controllers').controller('StatusController', function(activityFactory, apiService, badgesFactory, financesFactory, $scope, $q) {
+angular.module('calcentral.controllers').controller('StatusController', function(activityFactory, apiService, badgesFactory, financesFactory, holdsFactory, $scope, $q) {
   // Keep track on whether the status has been loaded or not
   var hasLoaded = false;
 
@@ -58,6 +58,21 @@ angular.module('calcentral.controllers').controller('StatusController', function
     }
   };
 
+  var loadHolds = function(data) {
+    if (!apiService.user.profile.features.csHolds) {
+      return;
+    }
+    $scope.holds = _.get(data, 'data.feed');
+    var numberOfHolds = _.get($scope, 'holds.serviceIndicators.length');
+    if (numberOfHolds) {
+      $scope.count += numberOfHolds;
+      $scope.hasAlerts = true;
+    } else if ($scope.holds.errored) {
+      $scope.count++;
+      $scope.hasWarnings = true;
+    }
+  };
+
   var finishLoading = function() {
     // Hides the spinner
     $scope.statusLoading = '';
@@ -80,9 +95,10 @@ angular.module('calcentral.controllers').controller('StatusController', function
       var getBadges = badgesFactory.getBadges().success(loadStudentInfo);
       var getFinances = financesFactory.getFinances().success(loadFinances);
       var getFinaidActivityOld = activityFactory.getFinaidActivityOld().then(loadActivity);
+      var getHolds = holdsFactory.getHolds().then(loadHolds);
 
       // Make sure to hide the spinner when everything is loaded
-      $q.all(getBadges, getFinances, getFinaidActivityOld).then(finishLoading);
+      $q.all(getBadges, getFinances, getFinaidActivityOld, getHolds).then(finishLoading);
     }
   });
 });
