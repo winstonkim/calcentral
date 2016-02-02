b8aaffb1effa818a3aa496238827d6a8a1d9cc4e
david kaye <dfkaye@yahoo.com>
2016-02-02 11:26:59 -0800

    SISRP-13260: first cut with WIP for photo & no-privilege text

--- src/assets/javascripts/angular/controllers/widgets/delegateStudentsController.js
+++ src/assets/javascripts/angular/controllers/widgets/delegateStudentsController.js
@@ -11,9 +11,37 @@ angular.module('calcentral.controllers').controller('DelegateStudentsController'
     isLoading: true
   };
 
+  var linkablePrivileges = [
+    'financial',
+    'viewEnrollments',
+    'viewGrades'
+  ];
+
+  /**
+   * TEMPORARY LOGIC ~ TO BE REPLACED BY BACK-END PROPERTY SETTING.
+   * setFakeStudentPhotoAPI() adds `profilePhoto` property as a fictious API for
+   * retrieving a delegate student's profile photo.
+   */
+  var setFakeStudentPhotoAPI = function(student) {
+    student.fakeProfilePhotoAPI = '/api/student/photo/' + student.uid;
+  };
+
+  /**
+   * setStudentViewability() adds `viewable` property on student, if and only if student
+   * has granted at least one delegate viewing privilege.
+   */
+  var setStudentViewability = function(student) {
+    var viewable = _.some(linkablePrivileges, function(key) {
+      return student.privileges[key];
+    });
+    student.viewable = viewable;
+  };
+
   var getStudents = function() {
     return delegateFactory.getStudents().then(function(data) {
       angular.extend($scope, _.get(data, 'data.feed'));
+      _.each($scope.students, setFakeStudentPhotoAPI);
+      _.each($scope.students, setStudentViewability);
       $scope.delegateStudents.isLoading = false;
     });
   };
