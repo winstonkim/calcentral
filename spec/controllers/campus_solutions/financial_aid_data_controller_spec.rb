18271442f1184324ac7d0639478c29d6f36403a7
John Crossman <johncrossman@berkeley.edu>
2016-02-03 22:03:41 -0800

    SISRP-14283, filter out EFC when advisor-view-as

--- spec/controllers/campus_solutions/financial_aid_data_controller_spec.rb
+++ spec/controllers/campus_solutions/financial_aid_data_controller_spec.rb
@@ -12,8 +12,25 @@ describe CampusSolutions::FinancialAidDataController do
       it 'has some field mapping info' do
         session['user_id'] = user_id
         get feed, {:aid_year => '2016', :format => 'json'}
-        json = JSON.parse(response.body)
+        json = JSON.parse response.body
         expect(json['feed']['coa']['title']).to eq 'Estimated Cost of Attendance'
+      end
+    end
+
+    context 'authenticated user' do
+      let(:filtered_feed) { { key: 'value' } }
+      before {
+        session['user_id'] = user_id
+        session['original_advisor_user_id'] = random_id
+        model = double(get_feed_as_json: filtered_feed)
+        expect(model).to receive(:aid_year=).with '2016'
+        expect(CampusSolutions::MyFinancialAidFilteredForAdvisor).to receive(:from_session).once.and_return model
+        expect(CampusSolutions::MyFinancialAidData).to_not receive :from_session
+      }
+      it 'invokes the filtered feed when advisor-view-as mode' do
+        get feed, {:aid_year => '2016', :format => 'json'}
+        json = JSON.parse response.body
+        expect(json['key']).to eq 'value'
       end
     end
   end
